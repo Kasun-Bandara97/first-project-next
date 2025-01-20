7966af99a8c65f8fc791a4abcc37bb69eb1bf5c2
Smart <knisansala14@gmail.com>
2025-01-20 22:12:46 +0530

    session p1

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,3 +1,7 @@
+import { getMovies } from "../libs/apis/server";
+import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
+import { IoIosStarHalf } from "react-icons/io";
 import {
   Card,
   CardContent,
@@ -6,8 +10,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { getMovies } from "../libs/apis/server";
-import Image from "next/image";
+
 
 export default async function DashboardPage() {
   const movies = await getMovies();
@@ -25,26 +28,44 @@ export default async function DashboardPage() {
         <div className="grid grid-cols-4 gap-4">
           {movies?.length &&
             movies.map((movie) => (
-              <div key={movie?._id} className="h-96">
-                <Card>
+              <div key={movie?._id} className="h-[500px]  ">
+                <Card className="h-full">
                   <CardHeader>
-                    <CardTitle>{movie?.title}</CardTitle>
-                    <CardDescription>{movie?.plot}</CardDescription>
+                    <CardTitle className="">{movie?.title} </CardTitle>
+                    <CardDescription className="text-xs ">
+                      {movie?.year ?? "N/A"}
+                    </CardDescription>
                   </CardHeader>
                   <CardContent>
-                    <div className="flex justify-center bg-black w-full h-[276px] mb-4 rounded">
+                    <div className="flex justify-center bg-black w-full h-[220px] mb-4 rounded">
                       <Image
                         src={movie?.poster}
                         alt={movie?.title}
                         width={200}
                         height={400}
                         className="h-full w-auto object-contain"
+                        priority={true}
                       />
                     </div>
+                    <div className="flex flex-col justify-between h-[154px]">
+                      <p className="line-clamp-3">{movie?.plot}</p>
+                      <div className="text-sm text-blue-900 font-semibold">
+                        {movie?.genres?.length && movie?.genres?.join(" / ")}
+                      </div>
+                      <div className="flex flex-row justify-between items-center">
+                        <Badge variant="success" className="font-medium">
+                          Rated: {movie?.rated ?? "N/A"}
+                        </Badge>
+                        <div className="flex flex-row gap-1 items-center" title="Imdb Rating">
+                          <IoIosStarHalf className="text-yellow-700"/>
+                          <span className="text-sm font-semibold">{movie?.imdb?.rating ?? 0}/10</span>
+                        </div>
+                      </div>
+                    </div>
                   </CardContent>
-                  <CardFooter>
+                  {/* <CardFooter>
                     <p>Card Footer</p>
-                  </CardFooter>
+                  </CardFooter> */}
                 </Card>
               </div>
             ))}
